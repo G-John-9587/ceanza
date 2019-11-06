e49255b526d34bacfe02cee9bd1f462bac766b84
G-John-9587 <360592051@qq.com>
2019-11-06 16:21:19 +0800

    jackson postman restful

--- src/main/java/cn/eyiya/ceanza/controller/TestController.java
+++ src/main/java/cn/eyiya/ceanza/controller/TestController.java
@@ -1,5 +1,9 @@
 package cn.eyiya.ceanza.controller;
 
+import cn.eyiya.ceanza.model.Test;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.omg.CORBA.IDLType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,12 @@ public class TestController {
 
     @RequestMapping("/test")
     public String Test(){
+        //@AllArgsConstructor //所有参数的构造函数
+        Test test = new Test(1L, "ttttt1");
+        //@Data //get、set方法
+        test.setTestName("aaaaa");
+        //@Builder //
+        Test test1 = Test.builder().id(2L).testName("bbbbb").build();
         return "Test spring boot";
     }
 }
---
+++ src/main/java/cn/eyiya/ceanza/model/Reader.java
@@ -0,0 +1,12 @@
+package cn.eyiya.ceanza.model;
+
+import lombok.Data;
+
+@Data
+public class Reader {
+
+    private String name;
+
+    private int age;
+
+}
