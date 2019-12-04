bde3828610f166fdc56a683f87fa5d1d4a5adb7b
G-John-9587 <360592051@qq.com>
2019-12-04 11:18:42 +0800

    自定义事件发布和监听

--- src/main/java/cn/eyiya/ceanza/CeanzaApplication.java
+++ src/main/java/cn/eyiya/ceanza/CeanzaApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class CeanzaApplication {
 
     public static void main(String[] args) {
-
         ConfigurableApplicationContext context = SpringApplication.run(CeanzaApplication.class, args);
         //获取context，手动装载监听器到上下文环境中
         context.addApplicationListener(new MyListener1());
